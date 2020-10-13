64a66672688527532dd1fd59695dfb48fabe39f1
io mintz <io@mintz.cc>
2020-10-13 03:23:07 +0000

    relax asks requirement to allow installing from pypi

--- setup.py
+++ setup.py
@@ -14,9 +14,8 @@ setuptools.setup(
     url="https://github.com/romainricard/signalstickers-client",
     packages=setuptools.find_packages(),
     install_requires=[
-        'anyio>=2.0.2,<3.0.0',
         # temporary until asks on pypi supports anyio 2.0
-        'asks @ git+https://github.com/theelous3/asks.git'
+        'asks>=2.4.10,<3.0.0',
         'cryptography>=3.1.1,<4.0.0',
         'protobuf>=3.13.0,<4.0.0',
     ],
