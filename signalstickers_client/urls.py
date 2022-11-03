95a6a8f9edc9dea943114b5af9df2e211c5e4000
Alex Sweet <sweet.giorni@gmail.com>
2022-11-03 10:52:42 -0700

    Update API url and CA cert

--- signalstickers_client/urls.py
+++ signalstickers_client/urls.py
@@ -5,5 +5,5 @@ CDN_STICKER_URL = CDN_BASEURL + "stickers/{pack_id}/"
 CDN_MANIFEST_URL = CDN_STICKER_URL + "manifest.proto"
 CDN_STICKER_URL = CDN_STICKER_URL + "full/{sticker_id}"
 
-SERVICE_BASEURL = "https://textsecure-service.whispersystems.org"
+SERVICE_BASEURL = "https://chat.signal.org"
 SERVICE_STICKER_FORM_URL = SERVICE_BASEURL + "/v1/sticker/pack/form/{nb_stickers}"
