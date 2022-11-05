8ae38d78caefd7eecf1d6d798986fb0fed8b69c8
Romain Ricard <romain.ricard@mines-ales.org>
2022-11-05 12:56:51 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signalstickers-client",
-    version="3.0.0",
+    version="3.2.0",
     author="Romain Ricard",
     author_email="contact+stickerclient@romainricard.fr",
     description="A client for the Signal stickers API",
